2ab162fdac4a559ad44cad981f1319ad6c7a20b9
Laiba <i200991@nu.edu.pk>
2023-05-15 17:41:25 +0500

    changes made

--- backend/server/route.js
+++ backend/server/route.js
@@ -1,2 +1,12 @@
 import express from 'express';
 import { createattendance, getallAttendance, getattendanceById, updateattendance, deleteattendance } from '../controller/attendance-controller.js';
+
+const router = express.Router();
+
+router.get('/', getallAttendance);
+router.post('/add', createattendance);
+router.get('/:id', getattendanceById);
+router.put('/:id', updateattendance);
+router.delete('/:id', deleteattendance);
+
+export default router;
